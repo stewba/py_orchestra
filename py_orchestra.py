017a1706ecc3b81e873c8036f334b1ca5fd88a02
Stewart Wilde <stew@mint>
2014-03-19 12:44:39 +1100

    Added simple midi output

--- py_orchestra.py
+++ py_orchestra.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
 # control a midi device to provide some backing to the current music
-# thanks to Chris Baume for the intro : 
+# audio detection using Chris Baume's guide to aubio : 
 # http://chrisbaume.wordpress.com/2013/02/09/aubio-alsaaudio/
+#
+# midi handled with python-rtmidi
+# http://trac.chrisarndt.de/code/wiki/python-rtmidi
+#
 
 import alsaaudio, struct
 from aubio.task import *
-import midi
+import rtmidi
  
 # constants
 CHANNELS    = 1
@@ -27,7 +31,24 @@ recorder.setperiodsize(FRAMESIZE)
 detect = new_aubio_pitchdetection(FRAMESIZE,FRAMESIZE/2,CHANNELS,
                                   RATE,PITCHALG,PITCHOUT)
 buf = new_fvec(FRAMESIZE,CHANNELS)
- 
+
+#setup midi out
+midiout = rtmidi.MidiOut()
+available_ports = midiout.get_ports()
+
+
+
+if available_ports:
+    counter = 1
+    for port in available_ports:
+        print "(",counter,") : ",port
+        counter = counter +1 
+    userPort = raw_input("Select port to open :")
+    midiout.open_port(int(userPort))
+else:
+    midiout.open_virtual_port("My virtual output")
+    print "opened a virtual port\n"
+
 # main loop
 runflag = 1
 while runflag:
@@ -50,3 +71,10 @@ while runflag:
  
   if(energy > 0.005): 
     print "{:10.4f} {:10.4f} {:10.0f}".format(freq,energy,note)
+    note_on = [0x90, note, 112] # channel 1, middle C, velocity 112
+    note_off = [0x80, note, 0]
+    midiout.send_message(note_on)
+    midiout.send_message(note_off)    
+
+
+
